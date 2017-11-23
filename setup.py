594e0b3e508651befc10b1cc2db7e82561d823b9
slazarov <s.a.lazarov@gmail.com>
2017-11-23 19:00:47 +0200

    Typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 install_requires = \
     [
-        'cfscrape==1.9.0',
+        #'cfscrape==1.9.0',
         'signalr-client==0.0.7'
     ]
 # 'requests[security]==2.18.4'#,
